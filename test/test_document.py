ba1aa07c5de075a5f6272e00e112a07b1083daf3
farooq-teqniqly <49829654+farooq-teqniqly@users.noreply.github.com>
2022-01-14 10:51:56 -0800

    Tests

--- test/test_document.py
+++ test/test_document.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 import pytest
 
 from dockie.core.document import Document
--- test/test_persistence.py
+++ test/test_persistence.py
@@ -1,19 +1,27 @@
 import os
+from typing import Optional
 
+import pytest
+
+from dockie.core import errors
 from dockie.core.database import Database
 from dockie.core.document import Document
 from dockie.core.persistence import persist_to_file, load_from_file
 
+db: Optional[Database] = None
+filename = os.path.join(os.getcwd(), "db.bak")
 
-def test_persist_database():
+
+def setup_module(module):
+    global db
     db = Database()
     db.add_container("orders")
 
     orders_container = db.get_container("orders")
     orders_container.add_document(Document("order1", {"customerId": 100}))
 
-    filename = os.path.join(os.getcwd(), "db.bak")
 
+def test_persist_database():
     if os.path.exists(filename):
         os.remove(filename)
 
@@ -31,3 +39,34 @@ def test_persist_database():
         )
     finally:
         os.remove(filename)
+
+
+def test_raise_error_when_file_exists():
+    if os.path.exists(filename):
+        os.remove(filename)
+
+    persist_to_file(db, filename)
+
+    try:
+        with pytest.raises(errors.PersistenceError):
+            persist_to_file(db, filename)
+    finally:
+        os.remove(filename)
+
+
+def test_overwrite_file():
+    if os.path.exists(filename):
+        os.remove(filename)
+
+    try:
+        persist_to_file(db, filename)
+        assert os.path.exists(filename)
+        persist_to_file(db, filename, overwrite=True)
+        assert os.path.exists(filename)
+    finally:
+        os.remove(filename)
+
+
+def test_load_from_file_raises_error_when_file_not_found():
+    with pytest.raises(errors.PersistenceError):
+        load_from_file("foo.bak")
